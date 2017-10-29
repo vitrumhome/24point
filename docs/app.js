d6db656a011911c1ab5e6be58b3f73e4c008ecf3
vitrum.zhu <vitrum.zhu@99bill.com>
2017-10-29 14:39:01 +0800

    更新文档

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,128 @@
-/**/
+/**
+ ** 加法函数，用来得到精确的加法结果
+ ** 说明：javascript的加法结果会有误差，在两个浮点数相加的时候会比较明显。这个函数返回较为精确的加法结果。
+ ** 调用：accAdd(arg1,arg2)
+ ** 返回值：arg1加上arg2的精确结果
+ **/
+function accAdd(arg1, arg2) {
+    var r1, r2, m, c;
+    try {
+        r1 = arg1.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r1 = 0;
+    }
+    try {
+        r2 = arg2.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r2 = 0;
+    }
+    c = Math.abs(r1 - r2);
+    m = Math.pow(10, Math.max(r1, r2));
+    if (c > 0) {
+        var cm = Math.pow(10, c);
+        if (r1 > r2) {
+            arg1 = Number(arg1.toString().replace(".", ""));
+            arg2 = Number(arg2.toString().replace(".", "")) * cm;
+        } else {
+            arg1 = Number(arg1.toString().replace(".", "")) * cm;
+            arg2 = Number(arg2.toString().replace(".", ""));
+        }
+    } else {
+        arg1 = Number(arg1.toString().replace(".", ""));
+        arg2 = Number(arg2.toString().replace(".", ""));
+    }
+    return (arg1 + arg2) / m;
+}
+ 
+//给Number类型增加一个add方法，调用起来更加方便。
+Number.prototype.add = function (arg) {
+    return accAdd(arg, this);
+};
+/**
+ ** 减法函数，用来得到精确的减法结果
+ ** 说明：javascript的减法结果会有误差，在两个浮点数相减的时候会比较明显。这个函数返回较为精确的减法结果。
+ ** 调用：accSub(arg1,arg2)
+ ** 返回值：arg1加上arg2的精确结果
+ **/
+function accSub(arg1, arg2) {
+    var r1, r2, m, n;
+    try {
+        r1 = arg1.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r1 = 0;
+    }
+    try {
+        r2 = arg2.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r2 = 0;
+    }
+    m = Math.pow(10, Math.max(r1, r2)); //last modify by deeka //动态控制精度长度
+    n = (r1 >= r2) ? r1 : r2;
+    return ((arg1 * m - arg2 * m) / m).toFixed(n);
+}
+ 
+// 给Number类型增加一个mul方法，调用起来更加方便。
+Number.prototype.sub = function (arg) {
+    return accMul(arg, this);
+};
+/**
+ ** 乘法函数，用来得到精确的乘法结果
+ ** 说明：javascript的乘法结果会有误差，在两个浮点数相乘的时候会比较明显。这个函数返回较为精确的乘法结果。
+ ** 调用：accMul(arg1,arg2)
+ ** 返回值：arg1乘以 arg2的精确结果
+ **/
+function accMul(arg1, arg2) {
+    var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+    try {
+        m += s1.split(".")[1].length;
+    }
+    catch (e) {
+    }
+    try {
+        m += s2.split(".")[1].length;
+    }
+    catch (e) {
+    }
+    return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+}
+ 
+// 给Number类型增加一个mul方法，调用起来更加方便。
+Number.prototype.mul = function (arg) {
+    return accMul(arg, this);
+};
+/** 
+ ** 除法函数，用来得到精确的除法结果
+ ** 说明：javascript的除法结果会有误差，在两个浮点数相除的时候会比较明显。这个函数返回较为精确的除法结果。
+ ** 调用：accDiv(arg1,arg2)
+ ** 返回值：arg1除以arg2的精确结果
+ **/
+function accDiv(arg1, arg2) {
+    var t1 = 0, t2 = 0, r1, r2;
+    try {
+        t1 = arg1.toString().split(".")[1].length;
+    }
+    catch (e) {
+    }
+    try {
+        t2 = arg2.toString().split(".")[1].length;
+    }
+    catch (e) {
+    }
+    with (Math) {
+        r1 = Number(arg1.toString().replace(".", ""));
+        r2 = Number(arg2.toString().replace(".", ""));
+        return (r1 / r2) * pow(10, t2 - t1);
+    }
+}
+ 
+//给Number类型增加一个div方法，调用起来更加方便。
+Number.prototype.div = function (arg) {
+    return accDiv(this, arg);
+};
 
     
     
@@ -29,23 +153,27 @@ function RPN(array) {
                     switch(array[i])
                     {
                     case '+':
-                        tempValue = parseFloat(array[i - 2]) + parseFloat(array[i - 1]) ;
+                        tempValue = accAdd( parseFloat(array[i - 2]) , parseFloat(array[i - 1]) );
+                        // tempValue = parseFloat(array[i - 2]) + parseFloat(array[i - 1]) ;
                         break;
                     case '-':
-                        tempValue = parseFloat(array[i - 2]) - parseFloat(array[i - 1]) ;
+                        tempValue = accSub( parseFloat(array[i - 2]) , parseFloat(array[i - 1]) );
+                        // tempValue = parseFloat(array[i - 2]) - parseFloat(array[i - 1]) ;
                         break;
                     case '*':
-                        tempValue = parseFloat(array[i - 2]) * parseFloat(array[i - 1]) ;
+                        tempValue = accMul( parseFloat(array[i - 2]) , parseFloat(array[i - 1]) );
+                        // tempValue = parseFloat(array[i - 2]) * parseFloat(array[i - 1]) ;
                         break;
                     case '/':
-                        tempValue = parseFloat(array[i - 2]) / parseFloat(array[i - 1]) ;
+                        tempValue = accDiv( parseFloat(array[i - 2]) , parseFloat(array[i - 1]) );
+                        // tempValue = parseFloat(array[i - 2]) / parseFloat(array[i - 1]) ;
                         break;
                     default:
                         break;
                     }
                     // console.log('tempValue',tempValue);
                     // tempValue = (new Function("","return "+tempExpWord))();
-                    if(tempValue<1 || !Number.isInteger(tempValue) ) {
+                    if( !Number.isInteger(tempValue) ) {
                         // console.log('hasDot(tempValue)',tempValue,hasDot(tempValue))
                         return false;
                     }else {
@@ -94,11 +222,43 @@ function getOpt(optList){
     // console.log("tempArr",tempArr)
     return tempArr;
 }
-
+let tranfromWord;
+function rRNP(array){
+    let tempValue = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] in operatorList){
+            if( (parseFloat(array[i - 2]) && parseFloat(array[i - 1]) ) ){
+                tranfromWord =  tranfromWord + ', ' + array[i - 2] + ' ' + array[i] + ' ' + array[i - 1];
+                tempValue = eval('' + parseFloat(array[i - 2]) + ' ' + array[i]+ ' ' + parseFloat(array[i - 1]));
+            }
+            array.splice(i - 2, 3, tempValue);
+        }
+    }
+    if(array.length>1){
+        // console.log(array);
+        return rRNP(array);
+    }else{
+        return array[0];
+    }
+}
+function tranfrom(info){
+    
+    
+    tranfromWord = info;
+    rRNP(info);
+    return tranfromWord;
+}
 function addLog(info){
     let infoBox = document.getElementById('console');
     let newWord = document.createElement("div");
-    newWord.innerHTML = info;
+    
+    // console.log(typeof info,'info', info);
+    if((typeof info) === 'object'){
+        console.log('info',info.length,info);
+        newWord.innerHTML = tranfrom(info);
+    }else{
+        newWord.innerHTML = info;
+    }
     infoBox.insertBefore(newWord,infoBox.childNodes[0]);
 }
 function upStatus(){
@@ -142,7 +302,7 @@ function timedCount(){
 
         // console.log('tempExp',tempExp,c, (new Date()).valueOf());
         if (RPN(tempExp) === 24){
-            console.log(backExp[c],c,'get 24');
+            // console.log(backExp[c],c,'get 24');
             addLog(backExp[c]);
         }else{
             tempExp = []; 
